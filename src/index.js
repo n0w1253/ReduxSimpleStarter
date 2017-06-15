2da9d9edb67a90a2847658ae2b3f879e4ffc8928
nancy <nancy@nancys-MacBook-Air.local>
2017-06-14 22:45:21 -0600

    es6 function syntax

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 // Create a new component.  This component should produce
 // some HTML
 
-const App = function () {  //const ES6
+const App = () => {  //const ES6
     return <div>Hi!</div>; //JSX, HTML looking JS, will be translated into regular JavaScript
 }
 
@@ -12,4 +12,4 @@ const App = function () {  //const ES6
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
-ReactDOM.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
